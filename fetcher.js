f9714dbbe1b5db5400bef29b921e603586becee4
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-18 10:01:07 -0500

    feat: writfile feature and checking the size

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,29 @@
 const argv = process.argv.slice(2);
+const { writeFile, statSync } = require("fs");
+const request = require("request");
+const readline = require("readline");
+// const { stdin: input, stdout: output } = process;
+// const rl = readline.createInterface({ input, output });
+const URL = argv[0];
+const FILE = argv[1];
 
-console.log(argv);
+request(URL, (error, response, body) => {
+  try {
+    writeFile(FILE, body, "utf8", () => {
+      const sizeInByte = statSync(FILE).size;
+      console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
+    });
+
+    // rl.question("Do you want to replace the existing file?", (answer) => {
+    //   if (answer === "yes") {
+    //     writeFile(FILE, body, "utf8", () => {
+    //       const sizeInByte = statSync(FILE).size;
+    //       console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
+    //     });
+    //   }
+    //   rl.close();
+    // });
+  } catch (error) {
+    console.log(error);
+  }
+});
