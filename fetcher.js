a24d94e1f46f580fa919ab06587652358b5fe1e7
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-18 11:03:11 -0500

    refactor: check if the file exist and if it exist it will ask if they want to replace the file

--- fetcher.js
+++ fetcher.js
@@ -1,28 +1,40 @@
 const argv = process.argv.slice(2);
-const { writeFile, statSync } = require("fs");
+const { writeFile, statSync, readFile, existsSync } = require("fs");
 const request = require("request");
 const readline = require("readline");
-// const { stdin: input, stdout: output } = process;
-// const rl = readline.createInterface({ input, output });
+const { stdin: input, stdout: output } = process;
+const rl = readline.createInterface({ input, output });
 const URL = argv[0];
 const FILE = argv[1];
+let currentFile;
 
 request(URL, (error, response, body) => {
+  if (error) {
+    console.log("Invalid credentials");
+    return;
+  }
+
+  if (existsSync(FILE)) {
+    rl.question(
+      "Do you want to replace the existing file? type 'y' if yes\n",
+      (answer) => {
+        if (answer === "y") {
+          writeFile(FILE, body, "utf8", () => {
+            const sizeInByte = statSync(FILE).size;
+            console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
+          });
+        }
+        rl.close();
+      }
+    );
+    return;
+  }
+
   try {
     writeFile(FILE, body, "utf8", () => {
       const sizeInByte = statSync(FILE).size;
       console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
     });
-
-    // rl.question("Do you want to replace the existing file?", (answer) => {
-    //   if (answer === "yes") {
-    //     writeFile(FILE, body, "utf8", () => {
-    //       const sizeInByte = statSync(FILE).size;
-    //       console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
-    //     });
-    //   }
-    //   rl.close();
-    // });
   } catch (error) {
     console.log(error);
   }
