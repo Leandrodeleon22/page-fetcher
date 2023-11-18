a30439f0a83bbdc00ce92d8cf5464dcd6aa0d293
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-18 13:01:11 -0500

    final

--- fetcher.js
+++ fetcher.js
@@ -1,18 +1,33 @@
 const argv = process.argv.slice(2);
-const { writeFile, statSync, readFile, existsSync } = require("fs");
+const { writeFile, statSync, readFile, existsSync, lstat } = require("fs");
 const request = require("request");
 const readline = require("readline");
+const path = require("path");
 const { stdin: input, stdout: output } = process;
 const rl = readline.createInterface({ input, output });
 const URL = argv[0];
 const FILE = argv[1];
-let currentFile;
 
 request(URL, (error, response, body) => {
   if (error) {
     console.log("Invalid credentials");
     return;
   }
+
+  //   lstat(FILE, (err, stats) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+
+  //     console.log(`Is file: ${stats.isFile()}`);
+  //     console.log(`Is directory: ${stats.isDirectory()}`);
+  //     console.log(`Is symbolic link: ${stats.isSymbolicLink()}`);
+  //     console.log(`Is FIFO: ${stats.isFIFO()}`);
+  //     console.log(`Is socket: ${stats.isSocket()}`);
+  //     console.log(`Is character device: ${stats.isCharacterDevice()}`);
+  //     console.log(`Is block device: ${stats.isBlockDevice()}`);
+  //   });
 
   if (existsSync(FILE)) {
     rl.question(
@@ -27,15 +42,13 @@ request(URL, (error, response, body) => {
         rl.close();
       }
     );
+
     return;
   }
 
-  try {
-    writeFile(FILE, body, "utf8", () => {
-      const sizeInByte = statSync(FILE).size;
-      console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  writeFile(FILE, body, "utf8", () => {
+    const sizeInByte = statSync(FILE).size;
+    console.log(`Downloaded and saved ${sizeInByte} bytes to ${FILE}`);
+    rl.close();
+  });
 });
